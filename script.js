ecd6161722caadc784e728e63c832be360a379a6
Elmi05 <karshe0548@gmail.com>
2023-12-05 20:02:16 +0300

    finished

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+    document.getElementById('count-btn').addEventListener('click', countVowels);
+
+    function countVowels() {
+        var inputText = document.getElementById('input-text').value.toLowerCase();
+
+        var vowelCount = 0;
+        for (var i = 0; i < inputText.length; i++) {
+            var currentChar = inputText[i];
+            if (isVowel(currentChar)) {
+                vowelCount++;
+            }
+        }
+
+        var resultElement = document.getElementById('vowel-count');
+        resultElement.innerHTML = ' ' + vowelCount;
+    }
+
+    function isVowel(char) {
+        return "aeiou".includes(char);
+    }
+
